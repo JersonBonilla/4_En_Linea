a9b3ccb7ab867447e993361429d11fedb9e69694
Jerson <jerson.bonilla.fer@gmail.com>
2024-06-11 21:06:42 -0600

    Todavia no he logrado hacer que funcione correctamente, los metodos de los jugadores si reciben emiten jugadas, pero al momento de usar la interfaz no emiten movimientos mas alla del primero si empiezan.

--- src/NewGameFrame.cc
+++ src/NewGameFrame.cc
@@ -50,24 +50,30 @@ void NewGameFrame::buildGame(wxString player1Name, wxString player2Name,
   switch (player1Type) {
     case 0:
       // Añadir cast de Persona
+      jugador1 = new JugadorHumano("jugador1", true);
       break;
     case 1:
       // Añadir cast de IAFacil
+      jugador1 = new IAFacil("jugador1", true);
       break;
     case 2:
       // Añadir cast de IADificil
+      jugador1 = new IAAvanzada("jugador1", true);
       break;
   }
 
   switch (player2Type) {
     case 0:
       // Añadir cast de Persona
+      jugador2 = new JugadorHumano("jugador2", false);
       break;
     case 1:
       // Añadir cast de IAFacil
+      jugador2 = new IAFacil("jugador2", false);
       break;
     case 2:
       // Añadir cast de IADificil
+      jugador2 = new IAAvanzada("jugador2", false);
       break;
   }
 
@@ -104,12 +110,12 @@ void NewGameFrame::OnWin() {
       this, wxString::Format("Ha ganado el jugador %d",
                              (tablero->getTurnos() % 2 == 0) ? 2 : 1));
   if (dialog.ShowModal() == wxID_OK) {
-    /*if (tablero->getTurnos() % 2 == 0){
+    if (tablero->getTurnos() % 2 == 0){
       jugador2->incrementarPartidasGanadas();
     }else{
       jugador1->incrementarPartidasGanadas();
     }
-    updateGame();*/
+    updateGame();
     SetStatusText("Tablero limpiado");
   } else {
     delete (jugador1NameLbl);
@@ -117,6 +123,8 @@ void NewGameFrame::OnWin() {
     delete (jugador2NameLbl);
     delete (jugador2WinsLbl);
     delete (canvas);
+    delete (jugador1);
+    delete (jugador2);
     showConfigurationDialog();
     SetStatusText("Bienvenido a 4 en linea!");
   }
@@ -135,6 +143,8 @@ void NewGameFrame::OnTie() {
     delete (jugador2NameLbl);
     delete (jugador2WinsLbl);
     delete (canvas);
+    delete (jugador1);
+    delete (jugador2);
     showConfigurationDialog();
     SetStatusText("Bienvenido a 4 en linea!");
   }
@@ -153,17 +163,18 @@ void NewGameFrame::OnPlayed(wxCommandEvent& event) {
     OnTie();
   } else {
     makePlay();
+
   }
 }
 
 // Limpia el tablero y actualiza las victorias del ultimo jugador que ganó.
 void NewGameFrame::updateGame() {
-  /*if(tablero->getTurnos%2==0){
+  if(tablero->getTurnos() % 2 == 0 ){
     jugador2WinsLbl->SetLabel(wxString::Format("Victorias: %d",
   jugador2->getPartidasGanadas())); }else{
     jugador1WinsLbl->SetLabel(wxString::Format("Victorias: %d",
   jugador1->getPartidasGanadas()));
-  }*/
+  }
   tablero->limpiarTablero();
   Refresh();
 }
@@ -173,17 +184,21 @@ void NewGameFrame::updateGame() {
 void NewGameFrame::makePlay() {
   int move = 0;
   if (tablero->getTurnos() == 0) {
-    // move = jugador1->movimientoIA(*tablero);
+    move = jugador1->movimientoIA(*tablero);
   } else if (tablero->getTurnos() % 2 == 0) {
-    // move = jugador2->movimientoIA(*tablero);
+    move = jugador1->movimientoIA(*tablero);
   } else {
-    // move = jugador1->movimientoIA(*tablero);
+    move = jugador2->movimientoIA(*tablero);
   }
-  /*if (move > -1){
-    tablero->LlenarCasilla(move, ((tablero->getTurnos() % 2 != 0) ?
-  Color::AMARILLO : Color::ROJO));
-  }*/
-  Refresh();
+  if (move > -1 && tablero->getTurnos() == 0){
+    tablero->LlenarCasilla(move, Color::ROJO);
+    Refresh();
+    wxCommandEvent playEvent(EVT_PLAYED);
+    playEvent.SetString("");
+    wxPostEvent(this, playEvent);
+  } else if (move > -1){
+    tablero->LlenarCasilla(move, (tablero->getTurnos() % 2 == 0) ? Color::AMARILLO : Color::ROJO);
+  }
 }
 
 void NewGameFrame::OnExit(wxCommandEvent& event) { Close(true); }
